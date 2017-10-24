455c9c60d8da9f9d4fa43855dc2d7184d2efefd0
dhirajdeshmukh <dhirajbdeshmukh@gmail.com>
2017-10-23 21:59:33 -0400

    Add files via upload

---
+++ ui.R
@@ -0,0 +1,23 @@
+library(shiny)
+library(ggplot2)
+library(dplyr)
+
+bcl <- read.csv("bcl-data.csv", stringsAsFactors = FALSE)
+
+ui <- fluidPage(
+  titlePanel("BC Liquor Store prices"),
+  sidebarLayout(
+    sidebarPanel(
+      sliderInput("priceInput", "Price", 0, 100, c(25, 40), pre = "$"),
+      radioButtons("typeInput", "Product type",
+                   choices = c("BEER", "REFRESHMENT", "SPIRITS", "WINE"),
+                   selected = "WINE"),
+      uiOutput("countryOutput")
+    ),
+    mainPanel(
+      plotOutput("coolplot"),
+      br(), br(),
+      tableOutput("results")
+    )
+  )
+)
