db92e57f35f183c2e9b5b8753b8f54c661e0f30c
dhirajdeshmukh <dhirajbdeshmukh@gmail.com>
2017-10-25 07:18:55 -0400

    Update ui_bmi.R

--- ui_bmi.R
+++ ui_bmi.R
@@ -1,3 +1,23 @@
-
+#ui.R
 library(shiny)
-shinyUI(fluidPage(  titlePanel("Body Mass Index (BMI) Calculator"),  sidebarLayout(sidebarPanel(    p('Body Mass Index (BMI) is a calculation that attempts to tell if you are underweight or overweight    for your height.'),    p('In the boxes on the left below, please enter your height and your weight. On the right,    you will see your calculated BMI measurement along with BMI chart.'),        radioButtons(      inputId  = "units",      label    = "Units:",      choices  = c("Metric (kg & m)" = 1, "Imperial (lb & in)" = 2),      selected = 1    ),        numericInput(      inputId = "mass",      label = strong("Enter your weight:"),      value = 70    ),        numericInput(      inputId = "height",      label = strong("Enter your height:"),      value = 1.80,      step  = 0.10    )    ),        mainPanel(      uiOutput("input"),      uiOutput("result"),      uiOutput("graph")    ))))
+shinyUI(fluidPage(  
+  titlePanel("Body Mass Index (BMI) Calculator"),  
+  sidebarLayout(sidebarPanel(    
+    p('Body Mass Index (BMI) is a measure of body fat based on your weight in relation to your height, and applies to most adult men and women aged 20 and over'),    
+    p('In the boxes below, please select the UNIT you want to enter your height and weight into and enter those in the boxes under UNIT section.'),
+    p('You will see your calculated BMI with BMI chart.'),        
+    radioButtons(inputId  = "units",      
+                 label    = "Units:",      
+                 choices  = c("Metric (kg & m)" = 1, "Imperial (lb & in)" = 2),      
+                 selected = 1    
+                ),        
+    numericInput(inputId = "mass",      
+                 label = strong("Enter your weight:"),      
+                 value = 70),        
+    numericInput(inputId = "height",      
+                 label = strong("Enter your height:"),      
+                 value = 1.80,      
+                 step  = 0.10)),        
+                mainPanel(uiOutput("input"),      
+                          uiOutput("result"),      
+                          uiOutput("graph")))))
